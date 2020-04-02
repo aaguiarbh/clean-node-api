f3164ea92ca58fa5565601c5a27eb70e053f0ea7
Alessandro Aguiar <alessandro.aguiar@gmail.com>
2020-04-02 20:26:41 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -16,7 +16,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'valid_email@mailcom',
+        email: 'valid_email@mail.com',
         password: 'valid_password'
       }
       return fakeAccount
@@ -183,7 +183,7 @@ test('Should call AddAccount with correct values', () => {
   })
 })
 
-test('Should return 500 if AddAccount throws', () => {
+test('Should return 400 if data is provided', () => {
   const { sut, addAccountStub } = makeSut()
   jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
     throw new Error()
@@ -199,4 +199,25 @@ test('Should return 500 if AddAccount throws', () => {
   const httpResponse = sut.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(500)
   expect(httpResponse.body).toEqual(new ServerError())
+})
+
+test('Should return 200 if an email is provided', () => {
+  const { sut } = makeSut()
+
+  const httpRequest = {
+    body: {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+      passwordConfirmation: 'valid_password'
+    }
+  }
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(200)
+  expect(httpResponse.body).toEqual({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'valid_password'
+  })
 })
